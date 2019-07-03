f430ddc247ee7e13d55f629f4c99d3b3f87f7003
Manhee <45567149+ItsJoeii@users.noreply.github.com>
2019-07-03 11:24:59 +0200

    Local Player

---
+++ Networking/Assets/Scripts/Player/PlayerWeapon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerWeapon
+{
+    public string name = "Yeet";
+
+    public float damage = 10f;
+    public float range = 100f;
+
+    public float fireRate = 0f;
+
+    public int maxBullets = 20;
+    [HideInInspector]
+    public int bullets;
+
+    public float reloadTime = 1f;
+
+    public GameObject graphics;
+
+    public PlayerWeapon()
+    {
+        bullets = maxBullets;
+    }
+}
---
+++ Networking/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float rotateSpeed = 5f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+    }
+}
